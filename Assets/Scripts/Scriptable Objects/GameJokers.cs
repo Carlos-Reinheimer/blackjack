e4957d8bff00aaeb4625d0f65f72e312a835c013
Carlos-Reinheimer <carlosgreinheimer@gmail.com>
2025-11-19 20:20:43 -0300

    add and organized new parameters for Joker Cards

--- Assets/Scripts/Scriptable Objects/GameJokers.cs
+++ Assets/Scripts/Scriptable Objects/GameJokers.cs
@@ -7,12 +7,20 @@ namespace Scriptable_Objects {
     
     [Serializable]
     public class JokerCard: BaseCard {
+        // identification parameters
         public string id;
         public JokerType jokerType;
+        
+        // Joker shop parameters
         public bool isUnlocked;
         public Sprite unlockedSprite;
         public Sprite lockedSprite;
         public int unlockPrice;
+        
+        // in-game parameters
+        [TextArea(10, 10)] public string actionDescription; // I think of this as something like Hades does, replacing parts of the text with icons or highlights that triggers other tooltips
+        public int purchasePrice;
+        public int availabilityAmount = 1;
         [SerializeReference, SubclassSelector] public IJoker iJoker;
     }
     
